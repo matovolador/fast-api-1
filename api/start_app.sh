5eaef33c2d1eaeb41e1676e633af3257e4296c24
Matias Garafoni <matias.garafoni@gmail.com>
2022-11-21 20:43:29 -0300

    start_app docker command

---
+++ api/start_app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#!/bin/bash
+alembic upgrade head || exit 1
+coverage run tests.py || exit 1
+coverage xml
+coverage lcov
+coverage html
+genbadge coverage -i - < /app/coverage/coverage.xml -o /app/coverage/coverage-badge.svg
+python main.py
