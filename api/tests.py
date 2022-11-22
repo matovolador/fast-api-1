bb685f7e9d428bb3ae586c721cf5ab266f1bad66
Matias Garafoni <matias.garafoni@gmail.com>
2022-11-21 22:46:45 -0300

    actual test data

--- api/tests.py
+++ api/tests.py
@@ -7,13 +7,42 @@ class TestIntegrationMain(TestCase):
     def setUp(self) -> None:
         self.client = TestClient(app)
 
-    @mock.patch('main.books', [])
-    def test_integration_givenValidBook_whenCreateBook_thenReturnCreatedBook(self) -> None:
-        valid_book = {
-            'isbn': 9780132350884,
-            'title': 'Clean Code: A Handbook of Agile Software Craftsmanship'
+    @mock.patch('main.products', [])
+    def test_create_product(self) -> None:
+        valid_product = {
+            "id": 1,
+            "name": "Standard-hilt lightsaber",
+            "uom": "pcs",
+            "category_name": "lightsaber",
+            "is_producible": True,
+            "is_purchasable": True,
+            "type": "product",
+            "purchase_uom": "pcs",
+            "purchase_uom_conversion_rate": 1,
+            "batch_tracked": False,
+            "variants": [
+                {
+                    "id": 1,
+                    "sku": "EM",
+                    "sales_price": 40,
+                    "product_id": 1,
+                    "purchase_price": 0,
+                    "type": "product",
+                    "created_at": "2020-10-23T10:37:05.085Z",
+                    "updated_at": "2020-10-23T10:37:05.085Z",
+                    "config_attributes": [
+                        {
+                            "config_name": "Type",
+                            "config_value": "Standard"
+                        }
+                    ]
+                }
+            ],
+            "additional_info": "additional info",
+            "created_at": "2020-10-23T10:37:05.085Z",
+            "updated_at": "2020-10-23T10:37:05.085Z"
         }
-        response = self.client.post('/books/', json=valid_book)
+        response = self.client.post('/v1/products/create', json=valid_product)
 
         self.assertEqual(200, response.status_code)
-        self.assertEqual(valid_book, response.json())
+        self.assertEqual(valid_product, response.json())
