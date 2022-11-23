07face271987cb552277520b981d7856b47d17bd
Matias Garafoni <matias.garafoni@gmail.com>
2022-11-23 03:19:24 -0300

    purchase_uom_conversion_rate cant be None if purchase_uom is not None

--- api/tests.py
+++ api/tests.py
@@ -289,3 +289,39 @@ if __name__ == "__main__":
         "updated_at": "2020-10-23T10:37:05.085Z"
     }
     tester.test_1_create_product(valid_product) # purchase_uom can be None
+
+
+    invalid_product = {
+        "id": 1,
+        "name": "Standard-hilt lightsaber 13 51 ",
+        "uom": "pcs",
+        "category_name": "lightsaber",
+        "is_producible": True,
+        "is_purchasable": True,
+        "type": "product",
+        "purchase_uom": "pcs",
+        "purchase_uom_conversion_rate": None,
+        "batch_tracked": False,
+        "variants": [
+            {
+                "id": 1,
+                "sku": "EM1asd",
+                "sales_price": 40,
+                "product_id": 1,
+                "purchase_price": 0,
+                "type": "product",
+                "created_at": "2020-10-23T10:37:05.085Z",
+                "updated_at": "2020-10-23T10:37:05.085Z",
+                "config_attributes": [
+                    {
+                        "config_name": "Type",
+                        "config_value": "Standard"
+                    }
+                ]
+            }
+        ],
+        "additional_info": "additional info",
+        "created_at": "2020-10-23T10:37:05.085Z",
+        "updated_at": "2020-10-23T10:37:05.085Z"
+    }
+    tester.test_1_create_product(invalid_product,should_fail=True) # purchase_uom_conversion_rate cant be None if purchase_uom is not None
