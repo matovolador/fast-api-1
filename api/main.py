848652d83d367156ad6cce4f36ab46f688c4e44d
Matias Garafoni <matias.garafoni@gmail.com>
2022-11-23 02:29:56 -0300

    rollback

--- api/main.py
+++ api/main.py
@@ -8,51 +8,21 @@ app = FastAPI()
 
 @app.post("/v1/products/create")
 def create_product(product: schemas.ProductCreate):
+    prod = False
+    inserted_variants = []
+    inserted_configs = []
     db = next(database.get_db())
-    prod = database.Product(
-        name = product.name,
-        uom = product.uom,
-        category_name = product.category_name,
-        is_producible = product.is_producible,
-        is_purchasable = product.is_purchasable,
-        type = 'product',
-        additional_info = product.additional_info,
-        purchase_uom = product.purchase_uom,
-        purchase_uom_conversion_rate = product.purchase_uom_conversion_rate,
-        batch_tracked = product.batch_tracked,
-        created_at = datetime.now(),
-        updated_at = datetime.now()
-    )
-    db.add(prod)
-    db.commit()
-    db.refresh(prod)
+            inserted_variants.append(variant)
+                    inserted_configs.append(config_attr)
 
-    for vari in product.variants:
-        variant = database.ProductVariant(
-            sku = vari.sku,
-            sales_price = vari.sales_price,
-            product_id = prod.id,
-            type = 'product',
-            created_at = datetime.now(),
-            updated_at = datetime.now()
-        )
-        db.add(variant)
+        for entry in inserted_configs:
+            db.delete(entry)
+        for entry in inserted_variants:
+            db.delete(entry)
+        if prod:
+            db.delete(prod)
         db.commit()
-        db.refresh(variant)
-
-        for config in vari.config_attributes:
-            config_attr = database.ConfigAttribute(
-                config_name = config.config_name,
-                config_value = config.config_value,
-                variant_id = variant.id 
-            )
-            db.add(config_attr)
-
-    
-    db.commit()
-    db.refresh(prod)
-    db.close()
-    return prod.as_dict()
+        db.close()
 
 
 if __name__ == "__main__":
