b2c79ae1d9b00f1826edfcbcba2bd42dc332ea0f
Matias Garafoni <matias.garafoni@gmail.com>
2022-11-23 03:18:58 -0300

    allow None purchase_uom test

--- api/tests.py
+++ api/tests.py
@@ -253,3 +253,39 @@ if __name__ == "__main__":
         "updated_at": "2020-10-23T10:37:05.085Z"
     }
     tester.test_1_create_product(invalid_product,should_fail=True) # purchase_uom is required
+
+
+    valid_product = {
+        "id": 1,
+        "name": "Standard-hilt lightsaber 891",
+        "uom": "pcs",
+        "category_name": "lightsaber",
+        "is_producible": True,
+        "is_purchasable": True,
+        "type": "product",
+        "purchase_uom": None,
+        "purchase_uom_conversion_rate": None,
+        "batch_tracked": False,
+        "variants": [
+            {
+                "id": 1,
+                "sku": "EM76",
+                "sales_price": 40,
+                "product_id": 1,
+                "purchase_price": 0,
+                "type": "product",
+                "created_at": "2020-10-23T10:37:05.085Z",
+                "updated_at": "2020-10-23T10:37:05.085Z",
+                "config_attributes": [
+                    {
+                        "config_name": "Type",
+                        "config_value": "Standard"
+                    }
+                ]
+            }
+        ],
+        "additional_info": "additional info",
+        "created_at": "2020-10-23T10:37:05.085Z",
+        "updated_at": "2020-10-23T10:37:05.085Z"
+    }
+    tester.test_1_create_product(valid_product) # purchase_uom can be None
